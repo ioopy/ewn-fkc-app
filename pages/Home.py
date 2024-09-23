84bf769fbdc63d226c0b7da13754bb29bdb5d26a
chitsanucha <pae.saythong@gmail.com>
2024-09-23 19:50:30 +0700

    add export pdf file

--- pages/Home.py
+++ pages/Home.py
@@ -8,3 +8,12 @@ hide_header_icons()
 st.title("เส้นไข่ขาว")
 st.write("")
 
+with open("data/ewn.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(label="Export Report",
+                    data=PDFbyte,
+                    file_name="ewn.pdf",
+                    mime='application/octet-stream')
+
+
